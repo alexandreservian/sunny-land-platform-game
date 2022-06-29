4476e13648b9c558002fea7275f70f8e2bc54b7f
alexandreservian <alexandreservian@gmail.com>
2022-06-28 22:42:52 -0300

    feat: Criando a barra de vida

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    public bool active = true;
+    [SerializeField] Sprite heartOff;
+    private Image heart;
+    private Sprite heartOn;
+
+    void Awake() {
+        heart = GetComponent<Image>();
+        heartOn = heart.sprite;
+        ActiveHeart(active);
+    }
+
+    public void ActiveHeart(bool active) {
+        heart.sprite = active ? heartOn : heartOff;
+    }
+
+}
